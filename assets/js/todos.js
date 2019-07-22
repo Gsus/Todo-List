a6402f76bfc8363940d8ce065ffc8dede9840b9d
Gsus <usjasb@gmail.com>
2019-07-21 23:23:10 -0400

    Done. Planning on adding Local Storage support next

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,11 @@
 // Check off specific todos by clicking
-$('ul').on('click', 'li', function(){
+$('ul').on('click', 'li', function () {
   $(this).toggleClass('completed');
 });
 
 // Click on X to delete Todo
-$('ul').on('click', 'span', function(e){
-  $(this).parent().fadeOut(500, function(){
+$('ul').on('click', 'span', function (e) {
+  $(this).parent().fadeOut(500, function () {
     $(this).remove();
   });
   // jQuery Method to stop Event Bubbling
@@ -13,13 +13,20 @@ $('ul').on('click', 'span', function(e){
 });
 
 // Add Todos
-$("input[type='text']").on('keypress', function(e){
-  if (e.which === 13) {    
+$("input[type='text']").on('keypress', function (e) {
+  if (e.which === 13) {
     // Grab text
     let todoText = $(this).val();
     // Reset input
     $(this).val("");
     // Add to ul
-    $('ul').append(`<li><span><i class="fas fa-trash"></i></span> ${todoText}</li>`);
+    $('ul').append(`<li><span><i class="fas fa-trash"></i></span>${todoText}</li>`);
   }
+});
+
+$(".fa-plus").on('click', function () {
+  // Fade in and out, and in case of fading in, focus as well.
+  $("input[type='text']").fadeToggle(function () {
+    $("input[type='text']").focus();
+  });
 });
